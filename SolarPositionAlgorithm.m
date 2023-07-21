61d52b475f06605b94dfe1f29c3028c3737953a3
Clancy Lin <poerevan9@gmail.com>
2023-07-22 01:23:44 +0800

    three col update

--- SolarPositionAlgorithm.m
+++ SolarPositionAlgorithm.m
@@ -1,7 +1,7 @@
 clc; clear;
 
-latitude = 23.5084;
 longitude = 120.8108;
+latitude = 23.5084;
 elevation = 2413.4;
 pressure = 1014;
 temperature = 25;
@@ -10,7 +10,7 @@ azm_rotation = 0;
 timezone = +8;
 
 current_date_time_utc = datetime('now', 'TimeZone', num2str(timezone, '+%d'));
-fmt = "yyyy-MM-dd-hh-mm-ss";
+fmt = 'yyyy-MM-dd-hh-mm-ss';
 str = string(current_date_time_utc, fmt);
 parts = str2double(strsplit(str, '-'));
 year = parts(1);
@@ -20,5 +20,15 @@ hour = parts(4);
 minute = parts(5);
 second = parts(6);
 
-[zenith, azimuth] = spa(year, month, day, hour, minute, second, timezone, ...
+[zenith, azimuth, sunrise, sunset] = spa(year, month, day, hour, minute, second, timezone, ...
     longitude, latitude, elevation, pressure, temperature, slope, azm_rotation);
+
+now = string(current_date_time_utc, 'hh:mm:ss');
+r = datenum(sunrise);
+s = datenum(sunset);
+n = datenum(now);
+if n > r && n < s
+    disp('Diurnal:       day')
+else
+    disp('Diurnal:       Nighttime')
+end
